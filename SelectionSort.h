cdab590cc34e89198c2e5c51835a5e6263f5f44a
Benjamin Chan <benjaminchan06@gmail.com>
2021-08-06 16:10:00 -0600

    Made asserts and reverse algorithm for SelectionSort.h

--- SelectionSort.h
+++ SelectionSort.h
@@ -19,6 +19,7 @@ public:
                     if (a[j] < a[min]) min = j;
                 }
                 exch(a, i, min);
+                assert(isSorted(a, 0, i, reverse));
             }
         } else {
             int n = a.size();
@@ -28,11 +29,17 @@ public:
                     if (a[j] > a[max]) max = j;
                 }
                 exch(a, i, max);
+                assert(isSorted(a, 0, i, reverse));
             }
         }
+        assert(isSorted(a, reverse));
     };
 private:
     static void exch(span<T> a, int i, int j);
+
+    static bool isSorted(span<T> a, int lo, int hi, bool reverse = false);
+
+    static bool isSorted(span<T> a, bool reverse = false);
 };
 
 template<typename T>
@@ -42,6 +49,28 @@ void SelectionSort<T>::exch(span<T> a, int i, int j) {
     a[i] = a[j];
     a[j] = swap;
 }
+
+template<typename T>
+requires Comparable<T>
+bool SelectionSort<T>::isSorted(span<T> a, bool reverse) {
+    return isSorted(a, 0, a.size() - 1, reverse);
+}
+
+
+template<typename T>
+requires Comparable<T>
+bool SelectionSort<T>::isSorted(span<T> a, int lo, int hi, bool reverse) {
+    if (!reverse) {
+        for (int i = 1; i <= hi; i++)
+            if (a[i] < a[i - 1]) return false;
+        return true;
+    } else {
+        for (int i = 1; i <= hi; i++)
+            if (a[i] > a[i - 1]) return false;
+        return true;
+    }
+}
+
 
 template<typename T> requires Comparable<T>
 SelectionSort(span<T>) -> SelectionSort<T>;

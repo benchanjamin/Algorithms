5d4fc8b0b36bf4a20d070452ccf2fb7e11c7dc5c
Benjamin Chan <benjaminchan06@gmail.com>
2021-08-10 21:39:13 -0600

    Comparable.h completed

--- Comparable.h
+++ Comparable.h
@@ -7,18 +7,35 @@
 
 #include <concepts>
 
+/**
+ * Checks if the From class is implicitly and explicitly convertible to the
+ * To class.
+ *
+ * @throws an error at compile-time if this constraint is violated
+ */
 template<class From, class To>
 concept convertible_to =
 std::is_convertible_v<From, To> && requires(From (&f)()) {
     static_cast<To>(f());
 };
 
+/**
+ * Checks if two classes are able to be compared with the "==" and "!=" operators
+ *
+ * @throws an error at compile-time if this constraint is violated
+ */
 template<typename T>
 concept Equal =requires(T a, T b) {
     { a == b } -> convertible_to<bool>;
     { a != b } -> convertible_to<bool>;
 };
 
+/**
+ * Checks if two classes are able to be compared with the ">", ">=", "<=", and "<"
+ * operators
+ *
+ * @throws an error at compile-time if this constraint is violated
+ */
 template<typename T>
 concept Comparable =
 Equal<T> && requires(T a, T b) {

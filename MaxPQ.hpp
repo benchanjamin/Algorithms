7aee4ea88a414625e61af35279fb8d09ca6c87c6
Benjamin Chan <benjaminchan06@gmail.com>
2021-08-26 02:05:16 -0500

    started comments

---
+++ MaxPQ.hpp
@@ -0,0 +1,34 @@
+//
+// Created by Ben Chan on 8/26/21.
+//
+
+#ifndef ALGORITHMS_MAXPQ_HPP
+#define ALGORITHMS_MAXPQ_HPP
+/**
+ *  The {@code MaxPQ} class represents a priority queue of generic keys.
+ *  It supports the usual <em>insert</em> and <em>delete-the-maximum</em>
+ *  operations, along with methods for peeking at the maximum key,
+ *  testing if the priority queue is empty, and iterating through
+ *  the keys.
+ *  <p>
+ *  This implementation uses a <em>binary heap</em>.
+ *  The <em>insert</em> and <em>delete-the-maximum</em> operations take
+ *  &Theta;(log <em>n</em>) amortized time, where <em>n</em> is the number
+ *  of elements in the priority queue. This is an amortized bound
+ *  (and not a worst-case bound) because of array resizing operations.
+ *  The <em>min</em>, <em>size</em>, and <em>is-empty</em> operations take
+ *  &Theta;(1) time in the worst case.
+ *  Construction takes time proportional to the specified capacity or the
+ *  number of items used to initialize the data structure.
+ *  <p>
+ *  For additional documentation, see
+ *  <a href="https://algs4.cs.princeton.edu/24pq">Section 2.4</a> of
+ *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ *
+ *  @author Robert Sedgewick
+ *  @author Kevin Wayne
+ *
+ *  @param <Key> the generic type of key on this priority queue
+ */
+
+#endif //ALGORITHMS_MAXPQ_HPP
